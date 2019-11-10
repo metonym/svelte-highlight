8b050a47d0320d5844b64d5070a2ff752961ff89
Eric Liu <ericyl.us@gmail.com>
2019-11-10 09:07:38 -0800

    docs(examples): add webpack example

---
+++ examples/webpack/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Highlight from 'svelte-highlight';
+  import { typescript } from 'svelte-highlight/languages';
+  import 'svelte-highlight/styles/github.css';
+</script>
+
+<Highlight language={typescript}>const a: number = 4;</Highlight>
---
+++ examples/webpack/src/index.js
@@ -0,0 +1,7 @@
+import App from './App.svelte';
+
+const app = new App({ target: document.body });
+
+window.app = app;
+
+export default app;
