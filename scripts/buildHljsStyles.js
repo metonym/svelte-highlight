36be8c2d56e05320ce844777b1c5ad7f7216f4ff
Eric Liu <ericyl.us@gmail.com>
2019-11-15 19:46:19 -0800

    chore(build-hljs-styles): rename constant

--- scripts/buildHljsStyles.js
+++ scripts/buildHljsStyles.js
@@ -10,7 +10,7 @@ async function buildHljsStyles() {
   await fs.mkdir('src/styles');
   await fs.mkdir('styles');
 
-  const supportedStyles = ['# Supported Styles\n'];
+  const md = ['# Supported Styles\n'];
 
   glob('node_modules/highlight.js/styles/*.css', {}, async (error, files) => {
     if (error) {
@@ -28,25 +28,25 @@ async function buildHljsStyles() {
         name = `_${name}`;
       }
 
-      supportedStyles.push(`## ${styleName} (\`${name}\`)\n`);
-      supportedStyles.push('<details>');
-      supportedStyles.push('<summary>Usage</summary>\n');
-      supportedStyles.push('### CSS Stylesheet\n');
-      supportedStyles.push('```html');
-      supportedStyles.push('<script>');
-      supportedStyles.push(`import 'svelte-highlight/styles/${name}.css';`);
-      supportedStyles.push('</script>');
-      supportedStyles.push('```\n');
-      supportedStyles.push('### JavaScript\n');
-      supportedStyles.push('```html');
-      supportedStyles.push('<script>');
-      supportedStyles.push(`import { ${name} } from 'svelte-highlight/styles';`);
-      supportedStyles.push('</script>\n');
-      supportedStyles.push('<svelte:head>');
-      supportedStyles.push(`{@html ${name}}`);
-      supportedStyles.push('</svelte:head>');
-      supportedStyles.push('```');
-      supportedStyles.push('</details>\n');
+      md.push(`## ${styleName} (\`${name}\`)\n`);
+      md.push('<details>');
+      md.push('<summary>Usage</summary>\n');
+      md.push('### CSS Stylesheet\n');
+      md.push('```html');
+      md.push('<script>');
+      md.push(`import 'svelte-highlight/styles/${name}.css';`);
+      md.push('</script>');
+      md.push('```\n');
+      md.push('### JavaScript\n');
+      md.push('```html');
+      md.push('<script>');
+      md.push(`import { ${name} } from 'svelte-highlight/styles';`);
+      md.push('</script>\n');
+      md.push('<svelte:head>');
+      md.push(`{@html ${name}}`);
+      md.push('</svelte:head>');
+      md.push('```');
+      md.push('</details>\n');
 
       baseExport.push(`export { default as ${name} } from './${styleName}';`);
       const buffer = await fs.readFile(file);
@@ -62,8 +62,8 @@ async function buildHljsStyles() {
     baseExport.push('\n');
     await fs.writeFile('src/styles/index.js', baseExport.join('\n'));
 
-    supportedStyles.push('\n');
-    await fs.writeFile('SUPPORTED_STYLES.md', supportedStyles.join('\n'));
+    md.push('\n');
+    await fs.writeFile('SUPPORTED_STYLES.md', md.join('\n'));
   });
 }
 
