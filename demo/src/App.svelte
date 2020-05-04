0f8448e435678055f92a5a9e1afe72ebd27260bb
Eric Liu <ericyl.us@gmail.com>
2020-05-03 20:15:01 -0700

    chore(demo): test three imports

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,5 +1,8 @@
 <script>
-  import Highlight, { HighlightSvelte } from "svelte-highlight";
+  import HighlightLegacy, {
+    Highlight,
+    HighlightSvelte
+  } from "svelte-highlight";
   import { typescript } from "svelte-highlight/languages";
   import { solarizedDark } from "svelte-highlight/styles";
 
@@ -24,10 +27,39 @@ const sum = add(1, 2);'}
 </Highlight>`;
 </script>
 
+<style>
+  :global(code) {
+    font-family: "Menlo", monospace;
+  }
+</style>
+
 <svelte:head>
   {@html solarizedDark}
 </svelte:head>
-
-<Highlight code={code_typescript} language={typescript} />
-
-<HighlightSvelte {code} />
+Legacy:
+<HighlightLegacy language={typescript}>{code_typescript}</HighlightLegacy>
+Highlight
+<Highlight
+  on:click={() => {
+    console.log('click');
+  }}
+  on:select={() => {
+    console.log('on:select');
+  }}
+  on:highlight={() => {
+    console.log('on:highlight', 'default');
+  }}
+  language={typescript}
+  code={code_typescript} />
+HighlightSvelte
+<HighlightSvelte
+  {code}
+  on:click={() => {
+    console.log('click');
+  }}
+  on:select={() => {
+    console.log('on:select');
+  }}
+  on:highlight={() => {
+    console.log('on:highlight', 'svelte');
+  }} />
