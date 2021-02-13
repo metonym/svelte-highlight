eb5135eee3a6633e1ac4a8bcc12cf9ce94e6405d
Eric Liu <ericyl.us@gmail.com>
2021-02-13 14:47:46 -0800

    docs: add rollup-typescript example

---
+++ examples/rollup-typescript/rollup.config.js
@@ -0,0 +1,31 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs"
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+
+const IS_PROD = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "src/main.ts",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    svelte({
+      emitCss: false,
+      preprocess: sveltePreprocess(),
+      compilerOptions: {
+        dev: !IS_PROD,
+      },
+    }),
+    commonjs(),
+    resolve({ browser: true, dedupe: ["svelte"] }),
+    typescript({ sourceMap: !IS_PROD }),
+    IS_PROD && terser(),
+  ]
+};
