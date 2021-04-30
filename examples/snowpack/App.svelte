f0671dbae87841598bb6d6485b7cfa1bdaa882ea
Eric Liu <ericyl.us@gmail.com>
2021-04-30 14:20:04 -0700

    chore: update examples

--- examples/snowpack/App.svelte
+++ examples/snowpack/App.svelte
@@ -1,13 +1,18 @@
 <script>
   import Highlight from "svelte-highlight";
-  import typescript from "svelte-highlight/src/languages/typescript";
+
+  // TODO: direct import fails to build
+  // only a wildcard import (*) will build correctly
+  import * as typescript from "svelte-highlight/src/languages/typescript";
   import atomOneDark from "svelte-highlight/src/styles/atom-one-dark";
 
   const code = "const add = (a: number, b: number) => a + b;";
+
+  console.log(typescript);
 </script>
 
 <svelte:head>
   {@html atomOneDark}
 </svelte:head>
 
-<Highlight language="{typescript}" code="{code}" />
+<Highlight language="{typescript.default}" code="{code}" />
