60be2ed092c1e9d9df3039e4dec3ce771c6ad66c
Eric Liu <ericyl.us@gmail.com>
2021-04-30 13:29:43 -0700

    chore: upgrade deps in demo

--- demo/src/lib/CodeSnippet.svelte
+++ demo/src/lib/CodeSnippet.svelte
@@ -2,7 +2,6 @@
   export let code = "";
 
   import { CodeSnippet } from "carbon-components-svelte";
-  import copy from "copy-to-clipboard";
 </script>
 
-<CodeSnippet {...$$restProps} code="{code}" on:click="{() => copy(code)}" />
+<CodeSnippet {...$$restProps} code="{code}" />
