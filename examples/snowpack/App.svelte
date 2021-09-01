072cb0dc06e33f8787c7d0b28a77168d2bad4cc1
metonym <ericyl.us@gmail.com>
2021-09-01 09:22:12 -0700

    chore(snowpack): use named language export

--- examples/snowpack/App.svelte
+++ examples/snowpack/App.svelte
@@ -1,18 +1,13 @@
 <script>
   import Highlight from "svelte-highlight";
-
-  // TODO: direct import fails to build
-  // only a wildcard import (*) will build correctly
-  import * as typescript from "svelte-highlight/src/languages/typescript";
+  import { typescript } from "svelte-highlight/src/languages/typescript";
   import atomOneDark from "svelte-highlight/src/styles/atom-one-dark";
 
   const code = "const add = (a: number, b: number) => a + b;";
-
-  console.log(typescript);
 </script>
 
 <svelte:head>
   {@html atomOneDark}
 </svelte:head>
 
-<Highlight language="{typescript.default}" code="{code}" />
+<Highlight language="{typescript}" code="{code}" />
