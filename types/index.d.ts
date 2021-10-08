ca0b58d1a6bbf5b5e86228776cf3b911b2958f1f
metonym <ericyl.us@gmail.com>
2021-10-08 16:00:47 -0700

    feat(types): use .svelte.d.ts extension for TypeScript definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-export { default as default } from "./Highlight";
-export { default as Highlight } from "./Highlight";
-export { default as HighlightAuto } from "./HighlightAuto";
-export { default as HighlightSvelte } from "./HighlightSvelte";
+export { default as default } from "./Highlight.svelte";
+export { default as Highlight } from "./Highlight.svelte";
+export { default as HighlightAuto } from "./HighlightAuto.svelte";
+export { default as HighlightSvelte } from "./HighlightSvelte.svelte";
