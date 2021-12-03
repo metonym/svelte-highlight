0cb99405163227b63d33c014e33d0220a6c6f118
metonym <ericyl.us@gmail.com>
2021-12-03 15:58:04 -0800

    refactor(scripts): use fs.rm instead of fs.rmdir

--- scripts/utils/fs.js
+++ scripts/utils/fs.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { promisify } from "util";
 
-export const rmdir = promisify(fs.rmdir);
+export const rmdir = promisify(fs.rm);
 export const mkdir = promisify(fs.mkdir);
 export const readFile = promisify(fs.readFile);
 export const writeFile = promisify(fs.writeFile);
