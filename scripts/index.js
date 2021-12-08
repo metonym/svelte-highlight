dfdbc38183150775a57f116aa88bb1a07ea2999f
metonym <ericyl.us@gmail.com>
2021-12-08 13:09:36 -0800

    refactor(scripts): use top-level await

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,9 @@ import { rmdir, mkdir } from "./utils/fs.js";
 import { buildLanguages } from "./build-languages.js";
 import { buildStyles } from "./build-styles.js";
 
-(async () => {
-  await rmdir("src/languages", { recursive: true, force: true });
-  await rmdir("src/styles", { recursive: true, force: true });
-  await mkdir("src/styles");
-  await mkdir("src/languages");
-  await buildLanguages();
-  await buildStyles();
-})();
+await rmdir("src/languages", { recursive: true, force: true });
+await rmdir("src/styles", { recursive: true, force: true });
+await mkdir("src/styles");
+await mkdir("src/languages");
+await buildLanguages();
+await buildStyles();
