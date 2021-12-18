a617ba8ba8dea08a89ed742ec5b0a74e08d2129a
Eric Liu <ericyl.us@gmail.com>
2021-12-18 07:12:34 -0800

    chore(scripts): use rmdirSync

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ import { rmdir, mkdir } from "./utils/fs.js";
 import { buildLanguages } from "./build-languages.js";
 import { buildStyles } from "./build-styles.js";
 
-await rmdir("src/languages", { recursive: true, force: true });
-await rmdir("src/styles", { recursive: true, force: true });
+rmdir("src/languages", { recursive: true, force: true });
+rmdir("src/styles", { recursive: true, force: true });
 await mkdir("src/styles");
 await mkdir("src/languages");
 await buildLanguages();
