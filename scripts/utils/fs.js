5203791fa9bb9ed8d5ac633cf4587fa5ebf6d441
metonym <ericyl.us@gmail.com>
2022-01-06 08:04:43 -0800

    refactor(scripts): use fs.rmSync instead of fs.rmdirSync

--- scripts/utils/fs.js
+++ scripts/utils/fs.js
@@ -3,10 +3,10 @@ import { promisify } from "util";
 
 export const mkdir = (dir) => {
   if (fs.existsSync(dir)) {
-    fs.rmdirSync(dir, { recursive: true });
+    fs.rmSync(dir, { recursive: true });
   }
   fs.mkdirSync(dir);
-}
+};
 export const readFile = promisify(fs.readFile);
 export const writeFile = promisify(fs.writeFile);
 export const copyFile = promisify(fs.copyFile);
