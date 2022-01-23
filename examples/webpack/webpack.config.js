2e2f40e7808f290211da34a5874d682707160e11
Eric Liu <ericyl.us@gmail.com>
2022-01-23 10:51:06 -0800

    chore(examples): fix broken webpack

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 const NODE_ENV = process.env.NODE_ENV || "development";
