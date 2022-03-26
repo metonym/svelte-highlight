aec44fb3fef52a796ec0df97e363b5c6fd02b141
Eric Liu <ericyl.us@gmail.com>
2022-03-26 08:36:57 -0700

    build: use sveltekit-package to build library

---
+++ tests/SvelteHighlightPackage.test.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Highlight from "../package";
+  import Highlight2 from "../package/Highlight.svelte";
+  import { typescript } from "../package/languages";
+  import javascript from "../package/languages/javascript";
+  import { github, purebasic, _3024 } from "../package/styles/index";
+  import "../package/styles/3024.css";
+</script>
+
+<Highlight code="" language={javascript || typescript} />
+
+<svelte:component this={Highlight2} />
+
+{github}
+{purebasic}
