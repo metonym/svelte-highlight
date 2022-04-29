917d53cb6b124586a270747df5140274a5bd890d
Eric Liu <ericyl.us@gmail.com>
2022-04-28 18:14:27 -0700

    chore(examples): bump deps [ci skip] [deploy skip]

--- examples/snowpack/App.svelte
+++ examples/snowpack/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { Highlight, HighlightAuto } from "svelte-highlight";
-  import typescript from "svelte-highlight/languages/typescript";
+  import { typescript } from "svelte-highlight/languages/typescript";
   import atomOneDark from "svelte-highlight/styles/atom-one-dark";
 
   const code = "const add = (a: number, b: number) => a + b;";
