9310c1bf52b31777ea150df90bd0e457f1d2bc4e
Eric Liu <ericyl.us@gmail.com>
2022-08-06 15:31:26 -0700

    test: assert language/style types copied to package

--- tests/SvelteHighlightPackage.test.svelte
+++ tests/SvelteHighlightPackage.test.svelte
@@ -2,14 +2,17 @@
   import Highlight from "../package";
   import Highlight2 from "../package/Highlight.svelte";
   import { typescript } from "../package/languages";
+  import typescriptDefault from "../package/languages/typescript";
+  import { typescript as ts } from "../package/languages/typescript";
   import javascript from "../package/languages/javascript";
   import { github, purebasic, _3024 } from "../package/styles/index";
+  import githubStyles from "../package/styles/github";
   import "../package/styles/3024.css";
 </script>
 
 <Highlight
   code=""
-  language={javascript || typescript}
+  language={javascript || typescript || typescriptDefault || ts}
   on:highlight={(e) => {
     console.log(e.detail);
   }}
@@ -21,4 +24,5 @@
 <svelte:component this={Highlight2} />
 
 {github}
+{githubStyles}
 {purebasic}
