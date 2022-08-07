c179b55e8e7774d7fc04a925fed53cb7c6353a14
metonym <ericyl.us@gmail.com>
2022-08-07 09:40:53 -0700

    docs: add dynamic import example for code-splitting (#234)
    * chore(examples): add `DynamicImport` component
    
    * docs: add section on code-splitting

---
+++ examples/webpack/src/DynamicImport.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let component;
+  let styles;
+
+  onMount(async () => {
+    component = (await import("svelte-highlight")).HighlightAuto;
+    styles = (await import("svelte-highlight/styles/atom-one-dark")).default;
+  });
+</script>
+
+<svelte:head>
+  {#if styles}
+    {@html styles}
+  {/if}
+</svelte:head>
+
+<svelte:component
+  this={component}
+  langtag
+  code={`body {\n  padding: 0;\n  color: red;\n}`}
+/>
