5c63fc08c90df04434c7b623caa0a21458da87a7
metonym <ericyl.us@gmail.com>
2022-08-15 16:34:31 -0700

    chore(examples): upgrade SvelteKit to latest

---
+++ examples/sveltekit/src/lib/DynamicImport.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { SvelteComponent } from "svelte";
+
+  let component: typeof SvelteComponent;
+  let styles: string;
+
+  onMount(async () => {
+    component = (await import("svelte-highlight")).HighlightAuto;
+    styles = (await import("svelte-highlight/styles/atom-one-dark")).default;
+  });
+</script>
+
+<svelte:head>
+  {#if styles}
+    {@html styles}
+  {/if}
+</svelte:head>
+
+<svelte:component
+  this={component}
+  langtag
+  code={`body {\n  padding: 0;\n  color: red;\n}`}
+/>
