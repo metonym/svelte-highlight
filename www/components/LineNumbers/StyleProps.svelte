e8347fd95e6d030d279d63472bc4b00b5d72a633
Eric Liu <ericyl.us@gmail.com>
2023-12-10 16:55:42 -0800

    docs: make the custom LineNumbers look nicer

--- www/components/LineNumbers/StyleProps.svelte
+++ www/components/LineNumbers/StyleProps.svelte
@@ -4,17 +4,17 @@
 
   const snippet = `<LineNumbers
     {highlighted}
-    --line-number-color="pink"
-    --border-color="rgba(255, 255, 255, 0.2)"
-    --padding-left={0}
+    --line-number-color="rgba(255, 255, 255, 0.3)"
+    --border-color="rgba(255, 255, 255, 0.1)"
+    --padding-left="2em"
     --padding-right="1em"
   />`;
 </script>
 
 <Basic
   {snippet}
-  --line-number-color="pink"
-  --border-color="rgba(255, 255, 255, 0.2)"
-  --padding-left={0}
+  --line-number-color="rgba(255, 255, 255, 0.3)"
+  --border-color="rgba(255, 255, 255, 0.1)"
+  --padding-left="2em"
   --padding-right="1em"
 />
