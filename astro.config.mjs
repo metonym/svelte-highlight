02238d742e506bfee1833f541456037c33477ab8
Eric Liu <ericyl.us@gmail.com>
2023-12-28 09:30:12 -0800

    chore(docs): use `vite.define` instead of a Svelte preprocessor

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,29 +9,15 @@ export default defineConfig({
   trailingSlash: "never",
   integrations: [
     svelte({
-      preprocess: [
-        vitePreprocess(),
-        optimizeImports(),
-        {
-          script: ({ content, filename }) => {
-            if (/node_modules/.test(filename)) return;
-
-            content = content.replace(
-              new RegExp("process.env.TS", "g"),
-              JSON.stringify(new Date().toLocaleString()),
-            );
-
-            return {
-              code: content,
-            };
-          },
-        },
-      ],
+      preprocess: [vitePreprocess(), optimizeImports()],
     }),
   ],
   srcDir: "./www",
   publicDir: "./www/public",
   vite: {
+    define: {
+      "process.env.TS": JSON.stringify(new Date().toLocaleString()),
+    },
     optimizeDeps: {
       // We use the `optimizeImports` preprocessor so we
       // don't need Vite to do redundant work here.
