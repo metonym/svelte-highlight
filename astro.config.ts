ce4363c67d61caffbce0489bf3e7e9c4c727a5aa
Eric Liu <ericyl.us@gmail.com>
2023-12-29 09:50:14 -0800

    chore(docs): use Vite prebundle instead of preprocessor

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
-import { optimizeImports } from "carbon-preprocess-svelte";
 import pkg from "./package.json" assert { type: "json" };
 
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   trailingSlash: "never",
   integrations: [
     svelte({
-      preprocess: [optimizeImports()],
+      prebundleSvelteLibraries: true,
     }),
   ],
   srcDir: "./www",
@@ -24,11 +23,6 @@ export default defineConfig({
       __PKG_HOMEPAGE: JSON.stringify(pkg.homepage),
       __PKG_HLJS_VERSION: JSON.stringify(pkg.dependencies["highlight.js"]),
       __TS: JSON.stringify(new Date().toLocaleString()),
-    },
-    optimizeDeps: {
-      // We use the `optimizeImports` preprocessor so we
-      // don't need Vite to do redundant work here.
-      exclude: ["carbon-components-svelte"],
     },
   },
 });
