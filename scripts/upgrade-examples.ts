292a33fc1d8665ac7f3978120edc8d5e7a2752f3
Eric Liu <ericyl.us@gmail.com>
2024-09-02 15:08:41 -0700

    refactor(scripts): specify mindepth [ci skip]

--- scripts/upgrade-examples.ts
+++ scripts/upgrade-examples.ts
@@ -1,5 +1,5 @@
 import { $ } from "bun";
 
-for await (const dir of $`find examples -maxdepth 1 -type d`.lines()) {
-  await $`cd ${dir} && bun update`;
+for await (const dir of $`find examples -maxdepth 1 -mindepth 1 -type d`.lines()) {
+  if (dir) await $`cd ${dir} && bun update`;
 }
