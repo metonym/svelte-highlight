d4b546eecfcb87ca9502b4686298bd6aeea08c02
Eric Liu <ericyl.us@gmail.com>
2024-09-21 17:05:42 -0700

    chore(examples): add scoped styles

--- examples/routify/src/routes/index.svelte
+++ examples/routify/src/routes/index.svelte
@@ -3,6 +3,7 @@
   import typescript from "svelte-highlight/languages/typescript";
   import atomOneDark from "svelte-highlight/styles/atom-one-dark";
   import DynamicImport from "../components/DynamicImport.svelte";
+  import ScopedStyles from "../components/ScopedStyles.svelte";
 
   const code = "const add = (a: number, b: number) => a + b;";
 </script>
@@ -14,3 +15,5 @@
 <Highlight language={typescript} {code} />
 
 <DynamicImport />
+
+<ScopedStyles />
