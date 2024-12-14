673b4ecefa771112a8ee4f7abfe6c3f2977ba696
Eric Liu <ericyl.us@gmail.com>
2024-12-14 15:18:49 -0800

    fix: discard duplicate selectors in CSS
    Fixes #356

--- scripts/utils/minify-css.ts
+++ scripts/utils/minify-css.ts
@@ -1,12 +1,16 @@
 import cssnano from "cssnano";
 import litePreset, { type LiteOptions } from "cssnano-preset-lite";
 import postcss from "postcss";
+import discardDuplicates from "postcss-discard-duplicates";
+import mergeRules from "postcss-merge-rules";
 
 export const minifyCss = (
   css: string,
   discardComments?: LiteOptions["discardComments"],
 ) => {
   return postcss([
+    discardDuplicates(),
+    mergeRules(),
     cssnano({ preset: litePreset({ discardComments }) }),
   ]).process(css).css;
 };
