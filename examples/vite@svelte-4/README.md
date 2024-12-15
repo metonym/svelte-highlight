825399acb8dde882319c0f6a5d62cca6175005c2
Eric Liu <ericyl.us@gmail.com>
2024-12-15 13:58:59 -0800

    chore(examples): rename `vite` --> `vite@svelte-4`

--- examples/vite@svelte-4/README.md
+++ examples/vite@svelte-4/README.md
@@ -1,4 +1,4 @@
-# examples/vite@svelte-5
+# examples/vite@svelte-4
 
 > `svelte-highlight` Vite set-up.
 
--- examples/vite@svelte-4/vite.config.ts
+++ examples/vite@svelte-4/vite.config.ts
@@ -2,9 +2,6 @@ import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import type { UserConfig } from "vite";
 
 export default {
-  resolve: {
-    conditions: ["browser"],
-  },
   plugins: [
     svelte({
       preprocess: vitePreprocess(),
