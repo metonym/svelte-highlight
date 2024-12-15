cf5627e4039af34bb94d894b31fb0da10860a1d2
Eric Liu <ericyl.us@gmail.com>
2024-12-15 13:33:04 -0800

    docs: update featured theme to `horizon-dark` [ci skip]

--- www/constants.ts
+++ www/constants.ts
@@ -1,3 +1,5 @@
+import type { ThemeModuleName, ThemeName } from "../scripts/utils/styles";
+
 export const PKG_NAME = __PKG_NAME;
 export const PKG_VERSION = __PKG_VERSION;
 export const PKG_HOMEPAGE = __PKG_HOMEPAGE;
@@ -5,5 +7,5 @@ export const PKG_HLJS_VERSION = __PKG_HLJS_VERSION;
 export const TS = __TS;
 
 // Change the featured theme used in the home page and languages pages.
-export const THEME_NAME = "ashes";
-export const THEME_MODULE_NAME = "ashes";
+export const THEME_NAME: ThemeName = "horizon-dark";
+export const THEME_MODULE_NAME: ThemeModuleName = "horizonDark";
