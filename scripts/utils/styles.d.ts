30447aa097e347acc227634461d68efe466b6c43
Eric Liu <ericyl.us@gmail.com>
2024-12-15 13:28:33 -0800

    chore(scripts): add utility to infer theme name

---
+++ scripts/utils/styles.d.ts
@@ -0,0 +1,11 @@
+import type * as styles from "../../src/styles";
+
+type KebabCase<S extends string> = S extends `${infer C}${infer T}`
+  ? T extends Uncapitalize<T>
+    ? `${Lowercase<C>}${KebabCase<T>}`
+    : `${Lowercase<C>}-${KebabCase<T>}`
+  : S;
+
+export type ThemeModuleName = keyof typeof styles;
+
+export type ThemeName = KebabCase<Theme>;
