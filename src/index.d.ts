0bf8580aae222b6d344254a44a2bb00ca4fea068
Eric Liu <ericyl.us@gmail.com>
2025-10-21 14:36:10 -0700

    feat(typescript): export `LanguageName` type
    Closes #385

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,4 +2,4 @@ export { default as Highlight, default as default } from "./Highlight.svelte";
 export { default as HighlightAuto } from "./HighlightAuto.svelte";
 export { default as HighlightSvelte } from "./HighlightSvelte.svelte";
 export { default as LineNumbers } from "./LineNumbers.svelte";
-export type { LanguageType } from "./languages";
+export type { LanguageType, LanguageName } from "./languages";
