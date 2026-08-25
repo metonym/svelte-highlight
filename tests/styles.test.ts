96b0632efc10470b5b105826367ef1b1a4cd79eb
Eric Liu <ericyl.us@gmail.com>
2026-08-24 19:24:02 -0700

    feat(styles): add custom `wet-plate` dark and light styles
    Collodion silver with iodine stain. Grayscale has no chemistry in it.

--- tests/styles.test.ts
+++ tests/styles.test.ts
@@ -5,6 +5,6 @@ test("Styles", () => {
 
   // @ts-expect-error
   expect(styles.default).toBeUndefined();
-  expect(styleNames.length).toEqual(270);
+  expect(styleNames.length).toEqual(272);
   expect(styleNames).toMatchSnapshot();
 });
