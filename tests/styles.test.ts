bf14db94c5a66b40b55e5acf3978a756c4225429
Eric Liu <ericyl.us@gmail.com>
2026-08-24 19:23:53 -0700

    feat(styles): add custom `cyanotype` dark and light styles
    Prussian blue sun print. Nord is gray-blue. This is 1842 iron chemistry.

--- tests/styles.test.ts
+++ tests/styles.test.ts
@@ -5,6 +5,6 @@ test("Styles", () => {
 
   // @ts-expect-error
   expect(styles.default).toBeUndefined();
-  expect(styleNames.length).toEqual(266);
+  expect(styleNames.length).toEqual(268);
   expect(styleNames).toMatchSnapshot();
 });
