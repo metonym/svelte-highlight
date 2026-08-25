2e93bfd89656beb39b3cb883ce3c3c7368f9b275
Eric Liu <ericyl.us@gmail.com>
2026-08-24 19:44:25 -0700

    feat(styles): add custom `lichen` dark and light styles
    Pale mint and dust-pink on stone.

--- tests/styles.test.ts
+++ tests/styles.test.ts
@@ -5,6 +5,6 @@ test("Styles", () => {
 
   // @ts-expect-error
   expect(styles.default).toBeUndefined();
-  expect(styleNames.length).toEqual(296);
+  expect(styleNames.length).toEqual(298);
   expect(styleNames).toMatchSnapshot();
 });
