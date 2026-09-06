484d5ebeb1bc736755b30a2607d4ec72b0c612b2
Eric Liu <ericyl.us@gmail.com>
2026-09-06 12:26:13 -0700

    feat(languages): add BAML language
    Adds a custom grammar, differential snippet, and language test for
    BAML.

--- tests/languages.test.ts
+++ tests/languages.test.ts
@@ -6,7 +6,7 @@ test("Languages", () => {
 
   // @ts-expect-error
   expect(languages.default).toBeUndefined();
-  expect(languageNames.length).toEqual(287);
+  expect(languageNames.length).toEqual(288);
   expect(languageNames).toMatchSnapshot();
 });
 
